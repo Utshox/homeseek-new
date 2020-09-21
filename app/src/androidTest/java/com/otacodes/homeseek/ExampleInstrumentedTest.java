1fdeacad0e8f0ed604b3ee27aac5a14dc33c2cbd
Istiaque Ahemd <iah.utsho@gmail.com>
2020-09-21 12:58:36 +0600

    latest and fittest v1

--- app/src/androidTest/java/com/otacodes/homeseek/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/otacodes/homeseek/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.otacodes.goestate", appContext.getPackageName());
+        assertEquals("com.diudefense.homeseek", appContext.getPackageName());
     }
 }
